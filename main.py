6390e523f9237b310d59b436a27e1427c08f3944
gvakhilesh <gvakhilesh.is21@rvce.edu.in>
2025-11-30 15:45:26 +0530

    temp

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate,load_prompt
 import os
+from langchain_pymupdf4llm import PyMuPDF4LLMLoader
+
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
@@ -18,7 +20,8 @@ prompt = PromptTemplate(
     input_variables = ['context', 'question']
 )
 
-
+file_path = "C:\\Users\\akhil\\Downloads\\pdf_chatbot\\NISM XXI A PMS Short Notes.PDF"
+loader = PyMuPDF4LLMLoader(file_path)
 
 result = model.invoke('What is the capital of India')
 
